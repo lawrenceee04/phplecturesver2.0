15b405e36a93e5026616a2c12dd0be4613097401
lawrenceee04 <lawrencelg04@gmail.com>
2024-09-28 22:15:22 +0800

    finished 4th lesson

--- 01-variables-data-types/04-strings-concatenation/index.php
+++ 01-variables-data-types/04-strings-concatenation/index.php
@@ -1,7 +1,10 @@
 <?php
-  $first_name = 'Maria';
-  $last_name = 'Clara';
-  $full_name = $first_name . ' ' . $last_name;
+$title = 'Learn PHP from Scratch';
+$heading = 'Strings and Concatenation';
+
+$first_name = 'Maria';
+$last_name = 'Clara';
+$full_name = $first_name . ' ' . $last_name;
 ?>
 
 <!DOCTYPE html>
@@ -11,44 +14,28 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
-  <title>Learn PHP From Scratch</title>
+  <title><?= $title ?></title>
 </head>
 
 <body class="bg-gray-100">
   <header class="bg-blue-500 text-white p-4">
     <div class="container mx-auto">
-      <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+      <h1 class="text-3xl font-semibold"><?= $title ?></h1>
     </div>
   </header>
-  <div class="container mx-auto p-4 mt-4">
+  <div class="container mx-auto p-4 m-4">
     <div class="bg-white rounded-lg shadow-md p-6">
-      <!-- Output -->
-
-      <p class="text-xl">
-      <?=
-       'Hello, my name is ' . $full_name;
-      ?>
-      </p>
-
-      <p class="text-xl">
-      <?=
-       "Hello, my name is $full_name";
-      ?>
-      </p>
-
-      <p class="text-xl">
-      <?=
-       'Hello, my name is $full_name';
-      ?>
-      </p>
-
-      <p class="text-xl">
-      <?=
-       "Hello, my name is {$full_name}";
-      ?>
-      </p>
-
+      <h2 class="text-2xl font-semibold"><?= $heading ?></h2>
     </div>
+  </div>
+
+  <div class="container mx-auto bg-white rounded-md shadow-md p-5 m-4">
+    <h3 class="text-xl font-semibold">First Name</h3>
+    <p><?= $first_name ?></p>
+    <h3 class="text-xl font-semibold">Last Name</h3>
+    <p><?= $last_name ?></p>
+    <h3 class="text-xl font-semibold">Full Name</h3>
+    <p><?= $full_name ?></p>
   </div>
 </body>
 
