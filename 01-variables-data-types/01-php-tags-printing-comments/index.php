bd738b2eb1c1b26b871c1bf20b136244ac61332d
lawrenceee04 <lawrencelg04@gmail.com>
2024-09-28 21:39:12 +0800

    finished 1st lesson

--- 01-variables-data-types/01-php-tags-printing-comments/index.php
+++ 01-variables-data-types/01-php-tags-printing-comments/index.php
@@ -1,11 +1,7 @@
 <?php
-    echo 'Hello World <br>';
-    echo 'Hello World';
-
-    echo 'Line 1', '<br>', 'Line 2', 'Line 3';
-
-    
+$lesson = 'PHP Tags, Printing, and Comments';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,21 +9,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title><?php echo 'Learn PHP From Scratch2' ?></title>
+    <title><?= 'Learn PHP From Scratch' ?></title>
 </head>
 
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold"><?= 'Learn PHP From Scratch3' ?></h1>
+            <h1 class="text-3xl font-semibold"><?= 'Learn PHP From Scratch' ?></h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4"><?= 'Welcome To The Bootcamp FEATI OOP'?></h2>
-            <p>In this bootcamp, you will learn the fundamentals of the PHP language</p>
+            <h2 class="text-2xl font-semibold"><?= $lesson ?></h2>
         </div>
     </div>
+
+    <div class=" container mx-auto bg-white rounded-md shadow-md p-5 m-4">
+
+        <?php
+        echo '1st Line, ';
+        echo 'Supposed to be 2nd Line (but it is not). <br>';
+
+        // echo in php will never print a new line
+        // therefore it is important to add <br> at the end of the echo statement to create a new line
+        echo '2nd Line <br>';
+        echo '3rd Line <br>';
+
+        // echo can also be used to print html tags
+        // as you can see here the p tag is printed using echo and it is rendered as a paragraph
+        echo '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, ipsam!</p>';
+
+        echo '<i>Heading 2</i> <br>';
+        echo '<strong>Heading 1</strong> <br>';
+        ?>
+
+    </div>
+
 </body>
 
 </html>
